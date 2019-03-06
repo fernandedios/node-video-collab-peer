caa870530c0ca0d1ddd5efd876a43d6086a57670
Fernan de Dios <info@mediagramstudios.com>
2019-03-06 11:34:03 +0800

    added port msg helper

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const ExpressPeerServer = require('peer').ExpressPeerServer;
 
 const PORT = process.env.PORT || 9000;
 const app = express();
+const msg = `Peer Server started at port ${PORT}`;
 
 const server = app.listen(PORT, (err) => {
   if (err) console.log(err);
-  console.log(`Peer Server started at port ${PORT}`);
+  console.log(msg);
 });
 
 const peerServer = ExpressPeerServer(server, { debug: true });
 
 app.use('/', peerServer);
+app.get('/port', function(req, res, next) {
+  res.send(msg);
+});
